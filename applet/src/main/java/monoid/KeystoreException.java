9dc88552c4cfe4ba148bdea7d8d52dbec4189a0a
vilicvane <id@wsh.earth>
2025-07-19 00:43:35 +0800

    Move exception code to static final

--- applet/src/main/java/monoid/KeystoreException.java
+++ applet/src/main/java/monoid/KeystoreException.java
@@ -4,14 +4,20 @@ public class KeystoreException extends MonoidException {
   public static final short REASON_KEY_NOT_FOUND = 1;
   public static final short REASON_INVALID_PARAMETER = 2;
 
+  public static final byte[] CODE_KEY_NOT_FOUND = {
+      'K', 'E', 'Y', '_', 'N', 'O', 'T', '_', 'F', 'O', 'U', 'N', 'D' };
+  public static final byte[] CODE_INVALID_PARAMETER = {
+      'I', 'N', 'V', 'A', 'L', 'I', 'D', '_', 'P', 'A', 'R', 'A', 'M', 'E', 'T', 'E', 'R' };
+
   public static Object[] codes;
 
   private static KeystoreException instance;
 
   public static void init() {
     codes = new Object[] {
-        new byte[] { 'K', 'E', 'Y', '_', 'N', 'O', 'T', '_', 'F', 'O', 'U', 'N', 'D' },
-        new byte[] { 'I', 'N', 'V', 'A', 'L', 'I', 'D', '_', 'P', 'A', 'R', 'A', 'M', 'E', 'T', 'E', 'R' } };
+        CODE_KEY_NOT_FOUND,
+        CODE_INVALID_PARAMETER
+    };
 
     instance = new KeystoreException();
   }
